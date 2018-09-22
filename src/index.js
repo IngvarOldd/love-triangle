38ca1a8442e7120302a1509a26dfa8590364d474
IngvarOldd <lyushinskij.i.a@gmail.com>
2018-09-23 00:25:20 +0300

    Second task 100%

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var answer = 0;
+  for (var i = 0; i < preferences.length; i++) {
+    var k = preferences[i];
+    var m = preferences[k-1];
+    if (preferences[m-1] == i+1) {
+      answer++;
+    }
+  }
+  return Math.floor(answer/3);
 };
